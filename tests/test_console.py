f18210928ae4fdc61af4b27d711417fee67dc29e
Paulo Costa <pcosta@firstrepublic.com>
2020-02-07 17:32:36 -0800

    Add mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,6 +7,15 @@ from python_project import console
 def runner():
     return click.testing.CliRunner()
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorep Ipsum",
+        "extract": "Lorep ipsum dolor sit amet",
+    }
+    return mock
+
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
