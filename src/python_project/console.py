c1df07fc2021fb24c2704d2093b7766f3910a803
Paulo Costa <pcosta@firstrepublic.com>
2020-02-10 14:45:23 -0800

    Add language option

--- src/python_project/console.py
+++ src/python_project/console.py
@@ -1,16 +1,22 @@
 import textwrap
 
-import locale
 import click
-import requests
 
 from . import __version__, wikipedia
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """A Python project."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
