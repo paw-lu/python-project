e82287aa4d38910446a2a0ed77aa180b469ba8e1
Paulo Costa <pcosta@firstrepublic.com>
2020-02-10 15:57:12 -0800

    Blacken noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,13 +3,12 @@ import nox
 locations = "src", "tests", "noxfile.py"
 nox.options.sessions = "lint", "tests"
 
+
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
-
-
 
 
 @nox.session(python=["3.8", "3.7"])
