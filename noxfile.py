2bcb05827582ae1a719ff59868432ed05e759a42
Paulo Costa <pcosta@firstrepublic.com>
2020-02-10 15:50:10 -0800

    Add flake8 nox

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,10 @@ def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+locations = "src", "tests", "noxfile.py"
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
