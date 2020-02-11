e1fc2d54166220d126ebfdd91c15ab399a288f26
Paulo Costa <pcosta@firstrepublic.com>
2020-02-11 12:59:38 -0800

    Add page attribute to wikipedia

--- src/python_project/wikipedia.py
+++ src/python_project/wikipedia.py
@@ -1,17 +1,30 @@
-from typing import Any
+from dataclasses import dataclass
 
 import click
+import desert
+import marshmallow
 import requests
+
+
+@dataclass
+class Page:
+    title: str
+    extract: str
+
+
+# Tell Marshmallow to ignore unknown fields via meta
+schema = desert.schema(Page, meta={"unknown": marshmallow.EXCLUDE})
 
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language: str = "en") -> Any:  # JSON hard to express as typ3
+def random_page(language: str = "en") -> Page:  # JSON hard to express as typ3
     url = API_URL.format(language=language)
     try:
         with requests.get(url) as response:
             response.raise_for_status()
-            return response.json()
-    except requests.RequestException as error:
+            data = response.json()
+            return schema.load(data)
+    except (requests.RequestException, marshmallow.ValidationError) as error:
         message = str(error)
         raise click.ClickException(message)
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,3 +1,8 @@
+from unittest.mock import Mock
+
+import click
+import pytest
+
 from python_project import wikipedia
 
 
@@ -5,3 +10,14 @@ def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
+
+
+def test_random_page_returns_page(mock_requests_get):
+    page = wikipedia.random_page()
+    assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_returns_validation_errors(mock_requests_get: Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
