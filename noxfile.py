799d57c0d80a29ddbde95c660b5b8a504511859d
Paulo Costa <pcosta@firstrepublic.com>
2020-02-11 10:44:15 -0800

    Let poetry manage test libraries

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,10 @@ def install_with_constraints(session, *args, **kwargs):
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
 
 
