51bf4502af78053775e78f8b79acac6fef7b2abb
Paulo Costa <pcosta@firstrepublic.com>
2020-02-11 10:03:49 -0800

    Add justification for full package installation

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,8 @@ def tests(session):
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
+    # Would typically just install flake8 and extensions, but isort
+    # needs to see which packages are installed
     session.run("poetry", "install", external=True)
     session.run("flake8", *args)
 
@@ -21,6 +23,8 @@ def lint(session):
 @nox.session(python="3.8")
 def sort(session):
     args = session.posargs or locations
+    # isort needs to see which packages are installed to properly sort
+    # imports
     session.run("poetry", "install", external=True)
     session.run("isort", "-rc", *args)
 
