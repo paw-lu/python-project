fe848b4b56a5707e56788a070bc814c880448a40
Paulo Costa <pcosta@firstrepublic.com>
2020-02-11 10:38:41 -0800

    Let poetry manage nox dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,22 @@
-import nox
 import tempfile
+
+import nox
 
 locations = "src", "tests", "noxfile.py"
 nox.options.sessions = "lint", "safety", "tests"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8", "3.7"])
@@ -17,23 +31,34 @@ def lint(session):
     args = session.posargs or locations
     # Would typically just install flake8 and extensions, but isort
     # needs to see which packages are installed
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    # Manually add non dev packages which are imported, like pytest
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-isort",
+        "pytest",
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def sort(session):
     args = session.posargs or locations
-    # isort needs to see which packages are installed to properly sort
-    # imports
-    session.run("poetry", "install", external=True)
+    # isort needs to see all installed packages that are imported
+    session.run("poetry", "install", "--no-dev", external=True)
+    # Manually add any non dev packages which are imported, like pytest
+    install_with_constraints(session, "pytest")
     session.run("isort", "-rc", *args)
 
 
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -49,5 +74,5 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
