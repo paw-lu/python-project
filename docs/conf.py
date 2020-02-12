ef5c78440811d5586a42267c263251cac094bb69
Paulo Costa <pcosta@firstrepublic.com>
2020-02-12 08:43:43 -0800

    Add Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "python-project"
 author = "Paulo S. Costa"
 copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
