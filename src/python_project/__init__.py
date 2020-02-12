b9c7b2e25d8e0a1ebb99a7164da59f4e51abce41
Paulo Costa <pcosta@firstrepublic.com>
2020-02-12 12:15:13 -0800

    Add automatic versioning

--- src/python_project/__init__.py
+++ src/python_project/__init__.py
@@ -1,2 +1,10 @@
 """A Python project."""
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version  # type: ignore
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
